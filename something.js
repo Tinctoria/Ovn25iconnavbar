167663fcc0a744463ea62f7e584ab3d84e75fc0a
Tinctoria <quizzie_89@hotmail.com>
2021-05-06 14:21:10 +0200

    first commit

---
+++ something.js
@@ -0,0 +1,9 @@
+function expandNav() {
+    var x=document.getElementsByClassName("list-content");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+}
+
